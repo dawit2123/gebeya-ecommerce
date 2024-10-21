29b9a298bd1f324cbb7c29c3a96e649e54c47f18
dawit2123 <dawitzewdu2123@gmail.com>
2024-10-22 01:32:46 +0300

    Refactor cloudinary configuration to use environment variables

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -1,10 +1,15 @@
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
+console.log(
+  process.env.CLOUDINARY_NAME,
+  process.env.CLOUDINARY_API_KEY,
+  process.env.CLOUDINARY_API_SECRET
+);
 
 cloudinary.config({
-  cloud_name: "",
-  api_key: "",
-  api_secret: "",
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new multer.memoryStorage();
