632e81f8a2109d04c6d9f61f7b39efbf7f97cdc7
dawit2123 <dawitzewdu2123@gmail.com>
2024-10-24 23:00:55 +0300

    project done

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://gebeyashop.vercel.app",
+    origin: "https://gebeyashop.onrender.com",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
