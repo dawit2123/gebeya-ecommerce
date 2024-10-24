e32f305e40398f928247de6759bdb148a39846a0
dawit2123 <dawitzewdu2123@gmail.com>
2024-10-24 20:26:24 +0300

    Refactor return and cancel URLs for PayPal integration

--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -26,8 +26,8 @@ const createOrder = async (req, res) => {
         payment_method: "paypal",
       },
       redirect_urls: {
-        return_url: "https://gebeya-bakend.vercel.app/shop/paypal-return",
-        cancel_url: "https://gebeya-bakend.vercel.app/shop/paypal-cancel",
+        return_url: "https://gebeyashop.vercel.app/shop/paypal-return",
+        cancel_url: "https://gebeyashop.vercel.app/shop/paypal-cancel",
       },
       transactions: [
         {
